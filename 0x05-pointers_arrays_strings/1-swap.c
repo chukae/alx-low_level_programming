6bfca32ecef7694ec8782ef77bc7881c26d2baba
Chuka E <chuka.ue@gmail.com>
2023-03-28 12:24:13 -0700

    Swap val

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * swap_int - Function swaps the values of two integers
+ *
+ * @a: pointer to integer value
+ * @b: pointer to integer value 
+ *
+ * Return: Always void.
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}
+
