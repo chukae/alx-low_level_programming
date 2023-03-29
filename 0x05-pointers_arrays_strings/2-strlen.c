d7e5b599ac9a34c5b0d6896408838a3ad0937600
Chuka E <chuka.ue@gmail.com>
2023-03-29 01:22:35 -0700

    str length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Reurns the length of a string.
+ *  @s: string
+ *
+ * Return: returns length of string as int.
+ **/
+int _strlen(char *s)
+{
+	int val = 0;
+	val = strlen(s);
+
+	return (val);
+}
+
