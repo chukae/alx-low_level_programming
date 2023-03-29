82c8afca62683326b760d782b258790c90c55dad
Chuka E <chuka.ue@gmail.com>
2023-03-29 04:26:26 -0700

    string newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ *
+ *@str: pointer variable to be reset
+ *
+ * Return: Always void.
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
